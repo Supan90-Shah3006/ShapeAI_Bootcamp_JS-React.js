cadbd89208084f9841ea2dfc2b050b142576e581
Supan90-Shah3006 <71488507+Supan90-Shah3006@users.noreply.github.com>
2021-06-07 10:04:37 +0530

    Add files via upload

---
+++ Note.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+function Note() {
+  return (
+
+   <div className="note">
+     <h2>Python And Machine Learning</h2>
+     <p>This is a Amazing Bootcamp of Python And Machine Learning Conducted By ShapeAI  </p>
+   </div>
+  );
+}
+
+export default Note;
